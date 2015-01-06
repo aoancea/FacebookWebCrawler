3ab12015a10dcc4677f45ddf59676519e2d325cd
aoancea <alx.oancea@gmail.com>
2015-01-07 00:11:28 +0200

    - Initial structure - Added DOM loading by URL and comment container filtering

---
+++ FacebookWebCrawler/FacebookWebCrawler/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace FacebookWebCrawler
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtUrl = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// txtUrl
+			// 
+			this.txtUrl.Location = new System.Drawing.Point(41, 19);
+			this.txtUrl.Name = "txtUrl";
+			this.txtUrl.Size = new System.Drawing.Size(533, 20);
+			this.txtUrl.TabIndex = 0;
+			this.txtUrl.Text = "https://www.facebook.com/klausiohannis";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.txtUrl);
+			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.groupBox1.Location = new System.Drawing.Point(5, 5);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(580, 83);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Action";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(23, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Url:";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(499, 58);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Process";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(590, 93);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.Padding = new System.Windows.Forms.Padding(5);
+			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtUrl;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ FacebookWebCrawler/FacebookWebCrawler/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace FacebookWebCrawler
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		HtmlDocument htmlDocument = null;
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			WebBrowser webBrowser = new WebBrowser();
+			webBrowser.Navigate(txtUrl.Text);
+
+			webBrowser.DocumentCompleted += (documentSender, documentEventArgs) =>
+			{
+				htmlDocument = webBrowser.Document;
+
+				HtmlElementCollection allDivCollection = htmlDocument.GetElementsByTagName("div");
+
+				List<HtmlElement> commentContainerCollection = new List<HtmlElement>();
+
+				foreach (HtmlElement htmlElement in allDivCollection)
+				{
+					if (htmlElement.GetAttribute("className").Contains("_4-u2 mbm _5jmm _5pat _5v3q"))
+					{
+						commentContainerCollection.Add(htmlElement);
+					}
+				}
+
+				var x = 1;
+
+			};
+		}
+	}
+}
