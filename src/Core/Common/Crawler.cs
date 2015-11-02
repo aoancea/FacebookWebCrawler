0dde5821ba42b8472f315bfd79ad9fc79c7ee4cb
aoancea <alx.oancea@gmail.com>
2015-11-02 20:31:19 +0200

    Crawler enhancements

--- src/Core/Common/Crawler.cs
+++ src/Core/Common/Crawler.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Crawler.Core.Common
 {
-    public class Crawler
-    {
-    }
+	public class Crawler
+	{
+		public Crawler()
+		{
+
+		}
+
+		public async Task<CrawlerQueryResult> ExecuteQueryAsync(string query)
+		{
+			//string queryPath = Crawler.QUERY_BASE_PATH + query + "/feed";
+			//if (queryPath.Contains('?'))
+			//{
+			//	queryPath += "&access_token=" + this.AccessToken;
+			//}
+			//else
+			//{
+			//	queryPath += "?access_token=" + this.AccessToken;
+			//}
+
+			string queryPath = string.Empty;
+
+			CrawlerQueryResult result = new CrawlerQueryResult();
+			WebClient client = new WebClient();
+
+			result.RawResult = await client.DownloadStringTaskAsync(queryPath);
+
+			return result;
+		}
+	}
 }
---
+++ src/Core/Common/CrawlerQueryResult.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Crawler.Core.Common
+{
+	public class CrawlerQueryResult
+	{
+		private string rawResult;
+		private JObject parsedJson;
+
+		public string RawResult
+		{
+			get
+			{
+				return this.rawResult;
+			}
+			set
+			{
+				this.rawResult = value;
+				parsedJson = JObject.Parse(this.rawResult);
+			}
+		}
+
+		public IEnumerable<string> GetField(string fieldName)
+		{
+			return parsedJson.SelectTokens(fieldName).Select(x => x.ToString());
+		}
+
+		public IEnumerable<JToken> GetFieldToken(string fieldName)
+		{
+			return parsedJson.SelectTokens(fieldName);
+		}
+
+		public string GetSingleField(string fieldName)
+		{
+			JToken fieldData = parsedJson.SelectToken(fieldName);
+			return fieldData != null ? fieldData.ToString() : null;
+		}
+	}
+}
