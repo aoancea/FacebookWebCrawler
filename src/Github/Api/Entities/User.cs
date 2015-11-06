183648af2f9ccf3867e4fc473c4932f73e72ab7e
aoancea <alx.oancea@gmail.com>
2015-11-06 08:03:24 +0200

    Added User entity

---
+++ src/Github/Api/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.Github.Api.Entities
+{
+	public class User
+	{
+		[DataMember(Name = "login")]
+		public string Login { get; set; }
+
+		[DataMember(Name = "id")]
+		public int Id { get; set; }
+
+		[DataMember(Name = "avatar_url")]
+		public string Avatar_Url { get; set; }
+
+
+	}
+}
