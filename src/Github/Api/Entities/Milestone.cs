df754d5d158a247eb90c9f0e5c50f2c057ecede2
aoancea <alx.oancea@gmail.com>
2015-11-15 17:16:09 +0200

    Added properties on Milestone entity

--- src/Github/Api/Entities/Milestone.cs
+++ src/Github/Api/Entities/Milestone.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,28 @@ namespace Crawler.Github.Api.Entities
 {
 	public class Milestone
 	{
+		[DataMember(Name = "url")]
+		public string Url { get; set; }
+
+		[DataMember(Name = "html_url")]
+		public string Html_Url { get; set; }
+
+		[DataMember(Name = "labels_url")]
+		public string Labels_Url { get; set; }
+
+		[DataMember(Name = "id")]
+		public int Id { get; set; }
+
+		[DataMember(Name = "number")]
+		public int Number { get; set; }
+
+		[DataMember(Name = "title")]
+		public int Title { get; set; }
+
+		[DataMember(Name = "description")]
+		public int Description { get; set; }
+
+		[DataMember(Name = "creator")]
+		public User Creator { get; set; }
 	}
 }
