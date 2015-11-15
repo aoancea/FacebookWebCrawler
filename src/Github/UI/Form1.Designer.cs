144d4f17b4195605c314578a0dc77cb5d85c1cb3
Ionescu Vlad <ionescu.vlad1@gmail.com>
2015-11-15 16:34:50 +0200

    Updated user interface with more options.

--- src/Github/UI/Form1.Designer.cs
+++ src/Github/UI/Form1.Designer.cs
@@ -28,13 +28,13 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+			System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
             "1",
-            "defect",
+            ".*defect.*",
             "bug"}, -1);
-			System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+			System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
             "2",
-            "problem",
+            ".*problem.*",
             "bug"}, -1);
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
@@ -57,6 +57,10 @@
 			this.progressBar1 = new System.Windows.Forms.ProgressBar();
 			this.button4 = new System.Windows.Forms.Button();
 			this.button5 = new System.Windows.Forms.Button();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.checkBox5 = new System.Windows.Forms.CheckBox();
+			this.checkBox6 = new System.Windows.Forms.CheckBox();
 			this.groupBox1.SuspendLayout();
 			this.groupBox2.SuspendLayout();
 			this.SuspendLayout();
@@ -104,6 +108,10 @@
 			// 
 			// groupBox1
 			// 
+			this.groupBox1.Controls.Add(this.checkBox6);
+			this.groupBox1.Controls.Add(this.checkBox5);
+			this.groupBox1.Controls.Add(this.checkBox4);
+			this.groupBox1.Controls.Add(this.checkBox3);
 			this.groupBox1.Controls.Add(this.checkBox2);
 			this.groupBox1.Controls.Add(this.checkBox1);
 			this.groupBox1.Controls.Add(this.button1);
@@ -113,7 +121,7 @@
 			this.groupBox1.Controls.Add(this.label2);
 			this.groupBox1.Location = new System.Drawing.Point(12, 12);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(583, 99);
+			this.groupBox1.Size = new System.Drawing.Size(583, 160);
 			this.groupBox1.TabIndex = 5;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Main options";
@@ -121,6 +129,8 @@
 			// checkBox1
 			// 
 			this.checkBox1.AutoSize = true;
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
 			this.checkBox1.Location = new System.Drawing.Point(11, 78);
 			this.checkBox1.Name = "checkBox1";
 			this.checkBox1.Size = new System.Drawing.Size(104, 17);
@@ -131,7 +141,7 @@
 			// checkBox2
 			// 
 			this.checkBox2.AutoSize = true;
-			this.checkBox2.Location = new System.Drawing.Point(145, 78);
+			this.checkBox2.Location = new System.Drawing.Point(11, 101);
 			this.checkBox2.Name = "checkBox2";
 			this.checkBox2.Size = new System.Drawing.Size(432, 17);
 			this.checkBox2.TabIndex = 7;
@@ -147,7 +157,7 @@
 			this.groupBox2.Controls.Add(this.button3);
 			this.groupBox2.Controls.Add(this.button2);
 			this.groupBox2.Controls.Add(this.listView1);
-			this.groupBox2.Location = new System.Drawing.Point(12, 117);
+			this.groupBox2.Location = new System.Drawing.Point(12, 193);
 			this.groupBox2.Name = "groupBox2";
 			this.groupBox2.Size = new System.Drawing.Size(583, 214);
 			this.groupBox2.TabIndex = 8;
@@ -163,10 +173,10 @@
 			this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.listView1.FullRowSelect = true;
 			this.listView1.GridLines = true;
-			listViewItem4.Tag = "";
+			listViewItem2.Tag = "";
 			this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem3,
-            listViewItem4});
+            listViewItem1,
+            listViewItem2});
 			this.listView1.Location = new System.Drawing.Point(6, 53);
 			this.listView1.Name = "listView1";
 			this.listView1.Size = new System.Drawing.Size(571, 123);
@@ -226,7 +236,6 @@
 			this.radioButton2.Name = "radioButton2";
 			this.radioButton2.Size = new System.Drawing.Size(266, 17);
 			this.radioButton2.TabIndex = 13;
-			this.radioButton2.TabStop = true;
 			this.radioButton2.Text = "If an issue has multiple labels, only consider the first";
 			this.radioButton2.UseVisualStyleBackColor = true;
 			// 
@@ -235,13 +244,13 @@
 			this.label3.AutoSize = true;
 			this.label3.Location = new System.Drawing.Point(3, 31);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(82, 13);
+			this.label3.Size = new System.Drawing.Size(167, 13);
 			this.label3.TabIndex = 8;
-			this.label3.Text = "Label renaming:";
+			this.label3.Text = "Label renaming (regex supported):";
 			// 
 			// progressBar1
 			// 
-			this.progressBar1.Location = new System.Drawing.Point(12, 337);
+			this.progressBar1.Location = new System.Drawing.Point(12, 413);
 			this.progressBar1.Name = "progressBar1";
 			this.progressBar1.Size = new System.Drawing.Size(583, 23);
 			this.progressBar1.TabIndex = 9;
@@ -249,7 +258,7 @@
 			// button4
 			// 
 			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button4.Location = new System.Drawing.Point(12, 366);
+			this.button4.Location = new System.Drawing.Point(12, 442);
 			this.button4.Name = "button4";
 			this.button4.Size = new System.Drawing.Size(83, 30);
 			this.button4.TabIndex = 14;
@@ -259,18 +268,64 @@
 			// button5
 			// 
 			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button5.Location = new System.Drawing.Point(101, 366);
+			this.button5.Location = new System.Drawing.Point(101, 442);
 			this.button5.Name = "button5";
 			this.button5.Size = new System.Drawing.Size(83, 30);
 			this.button5.TabIndex = 15;
 			this.button5.Text = "Cancel";
 			this.button5.UseVisualStyleBackColor = true;
 			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(129, 78);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(94, 17);
+			this.checkBox3.TabIndex = 9;
+			this.checkBox3.Text = "Group by label";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.AutoSize = true;
+			this.checkBox4.Checked = true;
+			this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox4.Location = new System.Drawing.Point(11, 137);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(112, 17);
+			this.checkBox4.TabIndex = 10;
+			this.checkBox4.Text = "Fetch open issues";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// checkBox5
+			// 
+			this.checkBox5.AutoSize = true;
+			this.checkBox5.Checked = true;
+			this.checkBox5.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox5.Location = new System.Drawing.Point(129, 137);
+			this.checkBox5.Name = "checkBox5";
+			this.checkBox5.Size = new System.Drawing.Size(119, 17);
+			this.checkBox5.TabIndex = 11;
+			this.checkBox5.Text = "Fetch closed issues";
+			this.checkBox5.UseVisualStyleBackColor = true;
+			// 
+			// checkBox6
+			// 
+			this.checkBox6.AutoSize = true;
+			this.checkBox6.Checked = true;
+			this.checkBox6.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox6.Location = new System.Drawing.Point(254, 137);
+			this.checkBox6.Name = "checkBox6";
+			this.checkBox6.Size = new System.Drawing.Size(140, 17);
+			this.checkBox6.TabIndex = 12;
+			this.checkBox6.Text = "Don\'t fetch pull requests";
+			this.checkBox6.UseVisualStyleBackColor = true;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(610, 402);
+			this.ClientSize = new System.Drawing.Size(610, 480);
 			this.Controls.Add(this.button5);
 			this.Controls.Add(this.button4);
 			this.Controls.Add(this.progressBar1);
@@ -312,6 +367,10 @@
 		private System.Windows.Forms.ProgressBar progressBar1;
 		private System.Windows.Forms.Button button4;
 		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.CheckBox checkBox6;
+		private System.Windows.Forms.CheckBox checkBox5;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.CheckBox checkBox3;
 	}
 }
 
